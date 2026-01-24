d2bad88e6e987592a929d53d18fcff731c3856ad
karolzebala <karol.d27@wp.pl>
2026-01-24 11:36:33 +0100

    add command injection

--- Apd.Api/Controllers/TestController.cs
+++ Apd.Api/Controllers/TestController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Adp.Infrastructure;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -16,7 +17,7 @@ public class TestController : ControllerBase
         _apdDbContext = apdDbContext;
     }
 
-    [HttpGet]
+    [HttpGet("users")]
     public IActionResult Get(string email)
     {
         var users = _apdDbContext.Users
@@ -24,5 +25,40 @@ public class TestController : ControllerBase
             .ToList();
         
         return Ok(users);
+    }
+    
+    [HttpGet("ping")]
+    public IActionResult PingHost(string hostname)
+    {
+        try
+        {
+            bool isWindows = OperatingSystem.IsWindows();
+    
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = isWindows ? "cmd.exe" : "/bin/bash",
+                    Arguments = isWindows
+                        ? $"/c ping {hostname}"
+                        : $"-c \"ping -c 4 {hostname}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+    
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+    
+            return Content(string.IsNullOrWhiteSpace(output) ? error : output, "text/plain");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 }
